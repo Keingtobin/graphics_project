32689f5044da317ef2046dc283c5578b823d5848
keingtobin <keingtobin@gmail.com>
2024-06-27 22:34:26 -0600

    most of tree body + lighting

--- main.c
+++ main.c
@@ -12,12 +12,17 @@ bool keyStates[inputKeys]; //[0]: W/w, [1]: A/a, [2]: S/s, [3] D/d
 double sx = 0;
 double sy = 0;
 double sz = 0;
-int th = 0;
-int ph = 30;
+int th = 0; // azimuth of viewangle
+int ph = 30; // elevation of view angle
+int mode = 0; // debug mode
+float ambient = 0.10;
+float diffuse = 0.50;
+float specular = 0.0;
+
 
 //helper function to handle key staetes
 void keyHandler(){
-    double moveAmt = 0.01;
+    double moveAmt = 0.001;
     if(keyStates[0] && !keyStates[1] && !keyStates[2] && !keyStates[3]){
         sx += moveAmt;
     }
@@ -33,27 +38,89 @@ void keyHandler(){
 }
 
 void display(){
-    //each display step, check key states.
-    keyHandler();
-    //eye position
-    double Ex = -2*dim*Sin(th)*Cos(ph) + sx;
-    double Ey = +2*dim        *Sin(ph) + sy;
-    double Ez = +2*dim*Cos(th)*Cos(ph) + sz;
-     //clear window and depth buffer
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    //enable depth buffering
-    glEnable(GL_DEPTH_TEST);
-    //load identity matrix
-    glLoadIdentity();
-    //set perspective
-    gluLookAt(Ex,Ey,Ez , sx,sy,sz , 0,Cos(ph),0);
-    glColor3f(1,1,1);
-    //try rotating before drawing
-    Sphere(sx,sy,sz,1);
-    Ground(1);
-    //do the displaying
-    glFlush();
-    glutSwapBuffers();
+    //debug mode
+    if(mode){
+        keyHandler();
+        double Ex = -2*dim*Sin(th)*Cos(ph);
+        double Ey = +2*dim        *Sin(ph);
+        double Ez = +2*dim*Cos(th)*Cos(ph);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+        glLoadIdentity();
+        gluLookAt(Ex,Ey,Ez,0,0,0,0,Cos(ph),0);
+        glShadeModel(GL_SMOOTH);
+        //lighting stuff
+        float Ambient[] = {ambient,ambient,ambient,1.0};
+        float Diffuse[] = {diffuse,diffuse,diffuse,1.0};
+        float Specular[] = {specular,specular,specular,1.0};
+        //set light position
+        float Position[] = {sx, sy, sz, 1.0};
+        //draw light ball
+        glColor3f(1,1,1);
+        Sphere(Position[0],Position[1],Position[2],0.1);
+        //tell opengl to normalize vectors
+        glEnable(GL_NORMALIZE);
+        //turn on the lights
+        glEnable(GL_LIGHTING);
+        //set ambient and diffuse color materials
+        glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
+        glEnable(GL_COLOR_MATERIAL);
+        glEnable(GL_LIGHT0);
+        //set ambient,diffuse,and specular components
+        glLightfv(GL_LIGHT0,GL_AMBIENT ,Ambient);
+        glLightfv(GL_LIGHT0,GL_DIFFUSE ,Diffuse);
+        glLightfv(GL_LIGHT0,GL_SPECULAR,Specular);
+        glLightfv(GL_LIGHT0,GL_POSITION,Position);
+        //draw stuff
+        Tree(0,0,0,0);
+
+        glDisable(GL_LIGHTING);
+        //draw axes
+        glColor3f(1,1,1);
+        const double len = 1.5;
+        glBegin(GL_LINES);
+        glVertex3d(0.0,0.0,0.0);
+        glVertex3d(len,0.0,0.0);
+        glVertex3d(0.0,0.0,0.0);
+        glVertex3d(0.0,len,0.0);
+        glVertex3d(0.0,0.0,0.0);
+        glVertex3d(0.0,0.0,len);
+        glEnd();
+        //  Label axes
+        glRasterPos3d(len,0.0,0.0);
+        Print("X");
+        glRasterPos3d(0.0,len,0.0);
+        Print("Y");
+        glRasterPos3d(0.0,0.0,len);
+        Print("Z");
+        //do the displaying
+        glFlush();
+        glutSwapBuffers();
+    }
+    else{
+        //each display step, check key states.
+        keyHandler();
+        //eye position
+        double Ex = -2*dim*Sin(th)*Cos(ph) + sx;
+        double Ey = +2*dim        *Sin(ph) + sy;
+        double Ez = +2*dim*Cos(th)*Cos(ph) + sz;
+        //clear window and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        //enable depth buffering
+        glEnable(GL_DEPTH_TEST);
+        //load identity matrix
+        glLoadIdentity();
+        //set perspective
+        gluLookAt(Ex,Ey,Ez , sx,sy,sz , 0,Cos(ph),0);
+        glColor3f(1,1,1);
+        //try rotating before drawing
+        Tree(0,0,0,0);
+        Ground(1);
+        //do the displaying
+        glFlush();
+        glutSwapBuffers();
+    }
+    
 }
 
 //gets called when window is resized.
@@ -67,7 +134,31 @@ void reshape(int width, int height){
 }
 
 void specialDown(int key, int x, int y){
-    
+    if(key == GLUT_KEY_RIGHT){
+        th+=5;
+    }
+    else if(key == GLUT_KEY_LEFT){
+        th -=5;
+    }
+    else if(key == GLUT_KEY_UP){
+        ph += 5;
+    }
+    else if(key == GLUT_KEY_DOWN){
+        ph -= 5;
+    }
+    else if(key == GLUT_KEY_PAGE_DOWN){
+        dim += 0.1;
+    }
+    else if(key == GLUT_KEY_PAGE_UP){
+        dim -= 0.1;
+    }
+    //keep angles +-360
+    th %= 360;
+    ph %= 360;
+    //update projection
+    Project(fov,asp,dim);
+    //redisplay the scene
+    glutPostRedisplay();
 }
 void specialUp(int key, int x, int y){
     
@@ -87,6 +178,22 @@ void keyDown(unsigned char key, int x, int y){
     }
     if(key == 'd' || key == 'D'){
         keyStates[3] = true;
+    }
+    if(key == 'm' || key == 'M'){
+        if(mode){
+            dim = 10;
+        }
+        else{
+            dim = 2;
+        }
+        mode = 1 - mode;
+        printf("Mode: %d", mode);
+    }
+    if(key == 'y'){
+        sy -= 0.05;
+    }
+    if(key == 'Y'){
+        sy += 0.05;
     }
     Project(fov,asp,dim);
     glutPostRedisplay();
--- objects.c
+++ objects.c
@@ -5,6 +5,32 @@
 //some colors used for material/lighting purposes
 float white[] = {1,1,1,1};
 float black[] = {0,0,0,1};
+typedef struct {float x,y,z;} vtx;
+typedef struct {int A,B,C,D;} quad;
+
+//inspired by triangle ex13
+static void Quad(vtx A, vtx B, vtx C, vtx D){
+    // planar vector 0
+    float dx0 = A.x-B.x;
+    float dy0 = A.y-B.y;
+    float dz0 = A.z-B.z;
+    // planar vector 1
+    float dx1 = C.x-A.x;
+    float dy1 = C.y-A.y;
+    float dz1 = C.z-A.z;
+    // Normal
+    float Nx = dy0*dz1 - dy1*dz0;
+    float Ny = dz0*dx1 - dz1*dx0;
+    float Nz = dx0*dy1 - dx1*dy0;
+    //draw quad
+    glNormal3f(Nx,Ny,Nz);
+    glBegin(GL_QUADS);
+    glVertex3f(A.x,A.y,A.z);
+    glVertex3f(B.x,B.y,B.z);
+    glVertex3f(C.x,C.y,C.z);
+    glVertex3f(D.x,D.y,D.z);
+    glEnd();
+}
 /*
  *  Draw vertex in polar coordinates and set normals
  * Taken from ex13
@@ -47,7 +73,7 @@ void Sphere(double x, double y, double z, double r){
 * texture - which texture do we want to use (will probably change later)
 * num - number of quads to use in the ground.
 */
-void Ground(int texture, int num){
+void Ground(int texture){
     glPushMatrix();
     glColor3f(0,0.5,0);
     glBegin(GL_QUADS);
@@ -60,12 +86,68 @@ void Ground(int texture, int num){
     glPopMatrix();
 }
 
+//tree data
+const int Ntree = 42;
+// tree index list
+const quad tree_idx[] = 
+{
+    //body of tree
+    {3,2,1,0}, {2,5,4,1}, {5,7,6,4}, {7,3,0,6},
+    {8,9,2,3}, {9,10,5,2}, {10,11,7,5}, {11,8,3,7},
+    {12,13,9,8},{13,14,10,9}, {14,15,11,10}, {15,12,8,11},
+    {16,17,13,12},{17,18,14,13},{18,19,15,14},{19,16,12,15},
+    {19,18,17,16}, //cap that puppy off
+    //start branching off - branch 1
+    {20,21,22,23}, //"base" of the branches
+    {20,21,25,24},{20,24,27,23},{23,27,26,22},{22,26,25,21},
+    {24,25,26,27},
+    {24,28,31,27},{27,31,30,26},{26,30,29,25},{25,29,28,24},
+    {28,29,30,31},
+    {32,33,34,35},
+    {32,33,37,36},{32,36,39,35},{35,39,38,34},{34,38,37,33},
+    {36,37,38,39},
+    //start branching off - branch 2
+    {20,21,41,40},{20,40,43,23},{23,43,42,22},{22,42,41,21},
+    {40,41,45,44},{40,44,47,43},{43,47,46,42},{42,46,45,41}
+
+
+};
+// tree vertex coords
+const vtx tree[] = 
+{ 
+    {0.0, 0.0, 0.0}, {0.25, 0.0, 0.0}, {0.20, 0.2, -0.05}, {0.05, 0.2, -0.05}, //0,1,2,3
+    {0.25, 0.0, -0.25}, {0.2,0.2,-0.2}, {0.0, 0.0, -0.25}, {0.05, 0.2, -0.2}, //4,5,6,7
+    {0.25, 0.55, -0.05}, {0.45, 0.55, -0.05}, {0.45, 0.55, -0.2}, {0.25, 0.55, -0.2}, //8,9,10,11
+    {0.05, 0.7, -0.05}, {0.25, 0.8, -0.05}, {0.25, 0.8, -0.25}, {0.05, 0.7, -0.25}, //12,13,14,15
+    {0.0, 0.8, 0.0},{0.25, 0.9, 0},{0.25, 0.9, -0.25},{0.0, 0.8, -0.25}, //16,17,18,19
+    {0.0, 0.8, -0.14},{0.0, 0.8, -0.25},{0.2, 0.8, -0.25},{0.2, 0.8, -0.1}, //20,21,22,23
+    {-0.3, 1.05, -0.45},{-0.3, 1.05, -0.5},{-0.2, 1.1,-0.5 },{-0.2, 1.1, -0.4}, //24,25,26,27
+    {-0.3, 1.25, -0.45},{-0.3, 1.25, -0.5},{-0.2, 1.25, -0.5},{-0.2, 1.25, -0.4}, //28,29,30,31
+    {-0.125,0.92, -0.34},{-0.125, 0.92, -0.37},{-0.125, 0.96, -0.37},{-0.125, 0.96, -0.34}, //32,33,34,35
+    {-0.5,0.92, -0.34},{-0.5, 0.92, -0.37},{-0.5, 0.96, -0.37},{-0.5, 0.96, -0.34}, //36,37,38,39
+    {0.25, 1.3, -0.4},{0.25, 1.3, -0.5}, {0.35, 1.3, -0.5},{0.35, 1.3, -0.4}, //40,41,42,43
+    {0.05, 1.5, -0.5}, {0.05, 1.5, -0.6}, {0.15, 1.5, -0.6}, {0.15, 1.5, -0.5} //44,45,46,47
+
+};
+
+
+
 void Tree(double x, double y, double z, double rotation){
-    //drawing trunk
+    //set up some colors we need
+    float brown[] = {0.5430, 0.2610, 0.0742, 1.0};
+    glMaterialf(GL_FRONT_AND_BACK,GL_SHININESS,1);
+    glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,brown);
+    glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,white);
+    glMaterialfv(GL_FRONT_AND_BACK,GL_EMISSION,black);   
+    glColor4fv(brown);
+     //drawing trunk
     glPushMatrix();
     //translate, scale, rotate
-    glTranslated(x,y,z,);
+    glTranslated(x,y,z);
     glRotated(rotation,0,1,0);
+    for(int i = 0; i < Ntree; i++){
+        Quad(tree[tree_idx[i].A],tree[tree_idx[i].B],tree[tree_idx[i].C],tree[tree_idx[i].D]);
+    }
 
     glPopMatrix();
 }
